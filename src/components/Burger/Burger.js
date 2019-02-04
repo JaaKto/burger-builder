8f16c2f99076ef5cecef0a8b42721434cdae28e6
JaaKto <33008680+JaaKto@users.noreply.github.com>
2019-02-04 23:40:47 +0100

    added multiple build controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIgredients = Object.keys(props.ingredients).map((igKey) => {
+  let transformedIgredients = Object.keys(props.ingredients)
+    .map((igKey) => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
          return <BurgerIngredient key={igKey + i} type={igKey} />;
       });
-    });
-  console.log(transformedIgredients);
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIgredients.length === 0) {
+    transformedIgredients = <p>Please start adding ingredients!</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
