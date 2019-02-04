7164f85e275dd203d19c8d55286b10016a4dddfb
JaaKto <33008680+JaaKto@users.noreply.github.com>
2019-02-04 22:57:12 +0100

    created simple dynamic burger

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import Auxi from '../../hoc/Auxi';
+import classes from './Layout.css'
+
 const layout = (props) => (
-  <div>Toolbar, SideDrawer, Backdrop</div>
-  <main>
-    props.children
-  </main>
+  <Auxi>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>
+      {props.children}
+    </main>
+  </Auxi>
 );
+
+export default layout;
