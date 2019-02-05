712c92d3e722a2b2b8982260788f1788dfb5ca27
JaaKto <33008680+JaaKto@users.noreply.github.com>
2019-02-05 23:39:31 +0100

    conected state to build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,16 +4,21 @@ import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-  { label: 'Salad', tye: 'salad' },
-  { label: 'Bacon', tye: 'bacon' },
-  { label: 'Cheese', tye: 'cheese' },
-  { label: 'Meat', tye: 'meat' }
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
 ];
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
     {controls.map(ctrl => (
-      <BuildControl key={ctrl.label} label={ctrl.label} />
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]} />
     ))}
   </div>
 );
